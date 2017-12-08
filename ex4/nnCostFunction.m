9d60813465d8583f0efc37085c2a1804fde60bc1
Jaya Shankar Parimi <parimisankar@users.noreply.github.com>
2017-12-08 16:39:57 -0500

    ex 4 part 2

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -73,40 +73,45 @@ end
 
 J = 1/m * sum(sum(-Y.*log(h) - (1-Y).*log(1-h)));
 
-regularator = (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2))) * (lambda/(2*m));
+reg = (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2))) * (lambda/(2*m));
 
-J = J + regularator;
+J = J + reg;
 
 % -------------------------------------------------------------
 for t = 1:m
-
-	% For the input layer, where l=1:
-	a1 = [1; X(t,:)'];
-
-	% For the hidden layers, where l=2:
-	z2 = Theta1 * a1;
+    %For the input layer
+	%l=1
+    a1 = [1; X(t,:)'];
+    
+    %For the hidden layers
+	%l=2
+    z2 = Theta1 * a1;
 	a2 = [1; sigmoid(z2)];
 
+    %For the output layer
+    %l=3
 	z3 = Theta2 * a2;
 	a3 = sigmoid(z3);
 
-	yy = ([1:num_labels]==y(t))';
+	yk = ([1:num_labels]==y(t))';
 	% For the delta values:
-	delta_3 = a3 - yy;
+	delta3 = a3 - yk;
 
-	delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
-	delta_2 = delta_2(2:end); % Taking out the bias row
-
-	% delta_1 is not calculated because we do not associate error with the input    
+	delta2 = (Theta2'*delta3).*[1; sigmoidGradient(z2)];
+    % Remove bias row delta20
+	delta2 = delta2(2:end); 
 
 	% Big delta update
-	Theta1_grad = Theta1_grad + delta_2 * a1';
-	Theta2_grad = Theta2_grad + delta_3 * a2';
+	Theta1_grad = Theta1_grad + delta2 * a1';
+	Theta2_grad = Theta2_grad + delta3 * a2';
 end
 
-Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
-Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
-
+Theta1_grad = (1/m) * Theta1_grad;
+reg = (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta1_grad = Theta1_grad + reg;
+Theta2_grad = (1/m) * Theta2_grad; 
+reg = (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
+Theta2_grad = Theta2_grad + reg;
 % =========================================================================
 
 % Unroll gradients
